7749a330c78fd75ae0da972e7bc30e960764d4c1
baydre_africa <baydreafrica@gmail.com>
2025-02-11 12:10:12 +0100

    implemented missing endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,6 +48,16 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    if book_id not in db.books:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "Book not found"},
+        )
+    return db.books[book_id]
+
+
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
     return JSONResponse(
